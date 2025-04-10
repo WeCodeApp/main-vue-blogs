c574c290126ae756f0db1d14e64fbc494445f513
Shaira Cruz <shairayvonnecruz@gmail.com>
2025-04-10 12:57:58 +0800

    Composition API Basics

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Homepage</h1>
+  <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+  <button @click="handleClick">Click Me</button>
+  <input type="text" v-model="name">
+  <button @click="age++">Add 1 to age</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    console.log('setup')
+
+    const name = ref('mario')
+    const age = ref(30)
+
+    const p =  ref(null)
+
+    const handleClick = () => {
+      name.value = 'luigi'
+      age.value = 40
+    }
+
+    return { name, age, handleClick, p }
+  },
+}
+</script>
